fe3f693b3bf361e9d698262a4108c1120149505c
Jordi <hokus15@users.noreply.github.com>
2020-08-27 13:24:35 +0200

    Step forward to be PEP8 compliant

--- gps_data.py
+++ gps_data.py
@@ -17,23 +17,26 @@ import time
 
 gpsd = None # setting the global variable
 
-#MQTT function for on_publish callback
+
+# MQTT function for on_publish callback
 def on_publish(client, userdata, mid):
     logger.debug("Publish message id: {}".format(mid))
     pass
 
+
 def on_connect(client, userdata, flags, rc):
-    if rc==0:
-        client.connected_flag=True #set flag
+    if rc == 0:
+        client.connected_flag=True  #set flag
         logger.info("Successfully connected to MQTT")
     else:
-        logger.error("Not connected to MQTT. Bad connection Returned code=",rc)
+        logger.error("Not connected to MQTT. Bad connection Returned code=", rc)
+
 
 class GpsPoller(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
-        global gpsd # bring it in scope
-        gpsd = gps.gps(mode=gps.WATCH_ENABLE) # starting the stream of info
+        global gpsd  # bring it in scope
+        gpsd = gps.gps(mode=gps.WATCH_ENABLE)  # starting the stream of info
         self.current_value = None
         self.running = True
 
@@ -44,51 +47,52 @@ class GpsPoller(threading.Thread):
             # gpsd info to clear the buffer
             next(gpsd)
 
+
 if __name__ == '__main__':
     logger = logging.getLogger('gps')
-    
-    console_handler = logging.StreamHandler() # sends output to stderr
+
+    console_handler = logging.StreamHandler()  # sends output to stderr
     console_handler.setFormatter(logging.Formatter("%(asctime)s %(name)-3s %(levelname)-8s %(message)s"))
     console_handler.setLevel(logging.DEBUG)
     logger.addHandler(console_handler)
     
     file_handler = logging.handlers.TimedRotatingFileHandler(os.path.dirname(os.path.realpath(__file__)) + '/gps_data.log',
-                                                    when='midnight',
-                                                    backupCount=15) # sends output to gps_data.log file rotating it at midnight and storing latest 15 days
+                                                             when='midnight',
+                                                             backupCount=15)  # sends output to gps_data.log file rotating it at midnight and storing latest 15 days
 
     file_handler.setFormatter(logging.Formatter("%(asctime)s %(name)-3s %(levelname)-8s %(message)s"))
     file_handler.setLevel(logging.INFO)
     logger.addHandler(file_handler)
 
     logger.setLevel(logging.DEBUG)
-    
+
     with open(os.path.dirname(os.path.realpath(__file__)) + '/gps_data.config.json') as config_file:
         config = json.loads(config_file.read())
-    
+
     broker_address = config['mqtt']['broker']
     port = int(config['mqtt']['port'])
     user = config['mqtt']['user']
     password = config['mqtt']['password']
     topic_prefix = config['mqtt']['topic_prefix']
 
-    gpsp = GpsPoller()   # create the GPS thread
-    
+    gpsp = GpsPoller()  # create the GPS thread
+
     try:
         logger.info("=== Script start ===")
-        
+
         # Start GPS Poller thread
         gpsp.start()
-        
+
         mqtt.Client.connected_flag = False
         # Create MQTT client
-        mqtt_client= mqtt.Client(client_id="gps-data-script", protocol=mqtt.MQTTv311, transport="tcp")
+        mqtt_client = mqtt.Client(client_id="gps-data-script", protocol=mqtt.MQTTv311, transport="tcp")
         # Assign callback functions
-        mqtt_client.on_publish = on_publish 
+        mqtt_client.on_publish = on_publish
         mqtt_client.on_connect = on_connect
         # Set tls
         mqtt_client.tls_set()
         # Set user and password
-        mqtt_client.username_pw_set(user,password)
+        mqtt_client.username_pw_set(user, password)
         # Enable MQTT logger
         mqtt_client.enable_logger(logger)
         # Start loop to process callbacks
@@ -97,7 +101,7 @@ if __name__ == '__main__':
         while not mqtt_client.connected_flag:
             try:
                 logger.debug("Trying to connect to MQTT server")
-                mqtt_client.connect(broker_address,port)
+                mqtt_client.connect(broker_address, port)
             except Exception as err:
                 logger.error("MQTT connection could not be established: {}, retrying... ".format(err), exc_info=False)
             time.sleep(5)
@@ -107,7 +111,7 @@ if __name__ == '__main__':
         max_accuracy = int(config['service']['min_accuracy'])
 
         sleep_time = int(config['service']['sleep'])
-        
+
         published_messages = 0
         while True:
             try:
@@ -124,31 +128,31 @@ if __name__ == '__main__':
                 if fix_accuracy < max_accuracy:
                     logger.debug("GPS position fixed with +/- {} m".format(fix_accuracy))
                     location.update({'latitude': gpsd.fix.latitude,
-                                'longitude': gpsd.fix.longitude,
-                                'gps_accuracy': fix_accuracy,
-                                'eps': gpsd.fix.eps, # Estimated Speed error
-                                'epx': gpsd.fix.epx, # Estimated longitude error
-                                'epy': gpsd.fix.epy, # Estimated latitude error
-                                'epv': gpsd.fix.epv, # Estimated altitude error
-                                'ept': gpsd.fix.ept, # Estimated time error
-                                'speed': gpsd.fix.speed, # m/s
-                                'climb': gpsd.fix.climb, 
-                                'track': gpsd.fix.track,
-                                'mode': gpsd.fix.mode
+                                     'longitude': gpsd.fix.longitude,
+                                     'gps_accuracy': fix_accuracy,
+                                     'eps': gpsd.fix.eps,  # Estimated Speed error
+                                     'epx': gpsd.fix.epx,  # Estimated longitude error
+                                     'epy': gpsd.fix.epy,  # Estimated latitude error
+                                     'epv': gpsd.fix.epv,  # Estimated altitude error
+                                     'ept': gpsd.fix.ept,  # Estimated time error
+                                     'speed': gpsd.fix.speed,  # m/s
+                                     'climb': gpsd.fix.climb, 
+                                     'track': gpsd.fix.track,
+                                     'mode': gpsd.fix.mode
                             })
                     if previous_latitude != 0 and previous_logitude != 0:
                         # Previous latitude and longitude data is useful to measure distance travelled between updates.
                         location.update({
-                            'platitude': previous_latitude, # Latitude got from previous read
-                            'plongitude': previous_logitude # Longitude got from previous read
+                            'platitude': previous_latitude,  # Latitude got from previous read
+                            'plongitude': previous_logitude  # Longitude got from previous read
                         })
                     previous_latitude = gpsd.fix.latitude
                     previous_logitude = gpsd.fix.longitude
-    
+
                     # Publish to MQTT
                     logger.debug("Publishing positon to MQTT...")
                     logger.debug("{}".format(json.dumps(location)))
-    
+
     #                logger.debug("%s satellites in view" % len(gpsd.satellites))
     #                for sat in gpsd.satellites:
     #                    logger.debug("    %r" % sat)
@@ -167,7 +171,6 @@ if __name__ == '__main__':
 
                 logger.debug("Waiting {} seconds...".format(sleep_time))
                 time.sleep(sleep_time)
-
     except (KeyboardInterrupt, SystemExit):
         # when you press ctrl+c
         pass
@@ -178,6 +181,6 @@ if __name__ == '__main__':
         mqtt_client.loop_stop()
         mqtt_client.disconnect()
         gpsp.running = False
-        gpsp.join()   # wait for the thread to finish what it's doing
+        gpsp.join()  # wait for the thread to finish what it's doing
         logger.info("{} location points published".format(published_messages))
         logger.info("=== Script end ===")
